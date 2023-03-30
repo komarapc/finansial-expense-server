bdd28d48f3efb327711b0820b088d3ad57050590
komar.izmi <komar.izmi@gmail.com>
2023-03-30 16:30:05 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
